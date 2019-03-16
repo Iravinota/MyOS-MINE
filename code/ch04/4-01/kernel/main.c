f4dec75cb052558fd4eebfafc7a3f8f4ec2d785f
Eric <wswangshuo@gmail.com>
2019-03-17 00:54:14 +0800

    Show 4 color band

--- code/ch04/4-01/kernel/main.c
+++ code/ch04/4-01/kernel/main.c
@@ -15,6 +15,43 @@
 
 void Start_Kernel(void)
 {
-	while(1)
-		;
+        /*帧缓存物理地址0xe000-0000映射到线性地址0xffff8000-00a00000和0xa0-0000处*/
+        int *addr = (int *)0xffff800000a00000;  /*每像素占用32-bit位宽（int）*/
+        int i;
+
+        for (i = 0; i < 1440 * 20; i++)
+        {
+                *((char *)addr + 0) = (char)0x00;
+                *((char *)addr + 1) = (char)0x00;
+                *((char *)addr + 2) = (char)0xff;
+                *((char *)addr + 3) = (char)0x00;
+                addr += 1;      /*addr是int型指针，+1跳过4 bytes*/
+        }
+        for (i = 0; i < 1440 * 20; i++)
+        {
+                *((char *)addr + 0) = (char)0x00;
+                *((char *)addr + 1) = (char)0xff;
+                *((char *)addr + 2) = (char)0x00;
+                *((char *)addr + 3) = (char)0x00;
+                addr += 1;
+        }
+        for (i = 0; i < 1440 * 20; i++)
+        {
+                *((char *)addr + 0) = (char)0xff;
+                *((char *)addr + 1) = (char)0x00;
+                *((char *)addr + 2) = (char)0x00;
+                *((char *)addr + 3) = (char)0x00;
+                addr += 1;
+        }
+        for (i = 0; i < 1440 * 20; i++)
+        {
+                *((char *)addr + 0) = (char)0xff;
+                *((char *)addr + 1) = (char)0xff;
+                *((char *)addr + 2) = (char)0xff;
+                *((char *)addr + 3) = (char)0x00;
+                addr += 1;
+        }
+
+        while (1)
+                ;
 }
