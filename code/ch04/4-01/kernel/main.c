ee86f1695f79fa8f760a5b047d77ac5f1fead2d7
Eric <wswangshuo@gmail.com>
2019-03-18 23:37:59 +0800

    update

--- code/ch04/4-01/kernel/main.c
+++ code/ch04/4-01/kernel/main.c
@@ -13,11 +13,18 @@
 *
 ***************************************************/
 
+#include "font.h"
+#include "printk.h"
+
 void Start_Kernel(void)
 {
         /*帧缓存物理地址0xe000-0000映射到线性地址0xffff8000-00a00000和0xa0-0000处*/
         int *addr = (int *)0xffff800000a00000;  /*每像素占用32-bit位宽（int）*/
-        int i;
+        int i, j;
+        int *left_top;
+        unsigned char *fontp = font_ascii['A'];
+        unsigned char bittest = 0x80;
+        unsigned char font_value;
 
         for (i = 0; i < 1440 * 20; i++)
         {
@@ -51,6 +58,32 @@ void Start_Kernel(void)
                 *((char *)addr + 3) = (char)0x00;
                 addr += 1;
         }
+
+        /*Print A*/
+        left_top = addr;
+        for (i = 0; i < 16; i++) {
+                addr = left_top + 1440 * i;
+                font_value = fontp[i];
+                bittest = 0x80;                 /*10000000b*/
+                for (j = 0; j < 8; j++) {
+                        if (font_value & bittest) {     /*front color*/
+                                *((char *)addr + 0) = (char)0x00;
+                                *((char *)addr + 1) = (char)0x00;
+                                *((char *)addr + 2) = (char)0xff;
+                                *((char *)addr + 3) = (char)0x00;
+                        } else {
+                                *((char *)addr + 0) = (char)0x00;
+                                *((char *)addr + 1) = (char)0xff;
+                                *((char *)addr + 2) = (char)0x00;
+                                *((char *)addr + 3) = (char)0x00;
+                        }
+                        addr += 1;
+                        bittest = bittest >> 1;
+                }
+        }
+
+        putchar((unsigned int *)0xffff800000a00000, 1440, 8, 80, 0x00ff0000, 0x0000ff00, 'B');
+        
 
         while (1)
                 ;
