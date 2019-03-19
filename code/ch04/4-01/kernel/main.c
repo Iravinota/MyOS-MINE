58b4aa2c4794eee3ec7cc2c80badb93102c10005
Eric <wswangshuo@gmail.com>
2019-03-19 21:20:39 +0800

    show sentence

--- code/ch04/4-01/kernel/main.c
+++ code/ch04/4-01/kernel/main.c
@@ -18,86 +18,56 @@
 
 void Start_Kernel(void)
 {
-        /*帧缓存物理地址0xe000-0000映射到线性地址0xffff8000-00a00000和0xa0-0000处*/
-        int *addr = (int *)0xffff800000a00000;  /*每像素占用32-bit位宽（int）*/
-        int i, j;
-        int *left_top;
-        unsigned char *fontp = font_ascii['A'];
-        unsigned char bittest = 0x80;
-        unsigned char font_value;
+	int *addr = (int *)0xffff800000a00000;
+	int i;
 
-        Pos.XResolution = 1440;
-        Pos.YResolution = 900;
+	Pos.XResolution = 1440;
+	Pos.YResolution = 900;
 
-        Pos.XPosition = 0;
-        Pos.YPosition = 0;
+	Pos.XPosition = 0;
+	Pos.YPosition = 0;
 
-        Pos.XCharSize = 8;
-        Pos.YCharSize = 16;
+	Pos.XCharSize = 8;
+	Pos.YCharSize = 16;
 
-        Pos.FB_addr = (int *)0xffff800000a00000;
-        Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
+	Pos.FB_addr = (int *)0xffff800000a00000;
+	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
 
-        for (i = 0; i < 1440 * 20; i++)
-        {
-                *((char *)addr + 0) = (char)0x00;
-                *((char *)addr + 1) = (char)0x00;
-                *((char *)addr + 2) = (char)0xff;
-                *((char *)addr + 3) = (char)0x00;
-                addr += 1;      /*addr是int型指针，+1跳过4 bytes*/
-        }
-        for (i = 0; i < 1440 * 20; i++)
-        {
-                *((char *)addr + 0) = (char)0x00;
-                *((char *)addr + 1) = (char)0xff;
-                *((char *)addr + 2) = (char)0x00;
-                *((char *)addr + 3) = (char)0x00;
-                addr += 1;
-        }
-        for (i = 0; i < 1440 * 20; i++)
-        {
-                *((char *)addr + 0) = (char)0xff;
-                *((char *)addr + 1) = (char)0x00;
-                *((char *)addr + 2) = (char)0x00;
-                *((char *)addr + 3) = (char)0x00;
-                addr += 1;
-        }
-        for (i = 0; i < 1440 * 20; i++)
-        {
-                *((char *)addr + 0) = (char)0xff;
-                *((char *)addr + 1) = (char)0xff;
-                *((char *)addr + 2) = (char)0xff;
-                *((char *)addr + 3) = (char)0x00;
-                addr += 1;
-        }
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
 
-        /*Print A*/
-        left_top = addr;
-        for (i = 0; i < 16; i++) {
-                addr = left_top + 1440 * i;
-                font_value = fontp[i];
-                bittest = 0x80;                 /*10000000b*/
-                for (j = 0; j < 8; j++) {
-                        if (font_value & bittest) {     /*front color*/
-                                *((char *)addr + 0) = (char)0x00;
-                                *((char *)addr + 1) = (char)0x00;
-                                *((char *)addr + 2) = (char)0xff;
-                                *((char *)addr + 3) = (char)0x00;
-                        } else {
-                                *((char *)addr + 0) = (char)0x00;
-                                *((char *)addr + 1) = (char)0xff;
-                                *((char *)addr + 2) = (char)0x00;
-                                *((char *)addr + 3) = (char)0x00;
-                        }
-                        addr += 1;
-                        bittest = bittest >> 1;
-                }
-        }
+	color_printk(YELLOW,BLACK,"Hello\t\t World!\n");
 
-        putchar((unsigned int *)0xffff800000a00000, 1440, 8, 80, 0x00ff0000, 0x0000ff00, 'B');
-        
-        color_printk(YELLOW, BLACK, "Hello\t\t World!\n");
-
-        while (1)
-                ;
+	while(1)
+		;
 }
