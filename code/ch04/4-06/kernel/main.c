812c92b842c69500f1737ca4325818bec034ddf8
Eric <wswangshuo@gmail.com>
2019-03-23 22:32:48 +0800

    update

--- code/ch04/4-06/kernel/main.c
+++ code/ch04/4-06/kernel/main.c
@@ -19,11 +19,22 @@
 #include "trap.h"
 #include "memory.h"
 
+/*
+Kernel.lds中设置的链接之后生成的system中各个段的起止标签，包括.text段（_text, _etext） .data段（_data, _edata） .bss段（_bss, _ebss） _end标记核心程序段结尾
+*/
+extern char _text;
+extern char _etext;
+extern char _edata;
+extern char _end;
+
 struct Global_Memory_Descriptor memory_management_struct = {{0},0};
+
 void Start_Kernel(void)
 {
 	int *addr = (int *)0xffff800000a00000;
 	int i;
+
+//	struct Page * page = NULL;
 
 	Pos.XResolution = 1440;
 	Pos.YResolution = 900;
@@ -35,7 +46,7 @@ void Start_Kernel(void)
 	Pos.YCharSize = 16;
 
 	Pos.FB_addr = (int *)0xffff800000a00000;
-	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
+	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4 + PAGE_4K_SIZE - 1) & PAGE_4K_MASK;
 
 	load_TR(8);
 
@@ -43,9 +54,29 @@ void Start_Kernel(void)
 
 	sys_vector_init();
 
+	memory_management_struct.start_code = (unsigned long)& _text;
+	memory_management_struct.end_code   = (unsigned long)& _etext;
+	memory_management_struct.end_data   = (unsigned long)& _edata;
+	memory_management_struct.end_brk    = (unsigned long)& _end;
+
 	color_printk(RED,BLACK,"memory init \n");
 	init_memory();
+/*
+	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*memory_management_struct.bits_map);
+	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*(memory_management_struct.bits_map + 1));
 
+	page = alloc_pages(ZONE_NORMAL,64,PG_PTable_Maped | PG_Active | PG_Kernel);
+
+	for(i = 0;i <= 64;i++)
+	{
+		color_printk(INDIGO,BLACK,"page%d\tattribute:%#018lx\taddress:%#018lx\t",i,(page + i)->attribute,(page + i)->PHY_address);
+		i++;
+		color_printk(INDIGO,BLACK,"page%d\tattribute:%#018lx\taddress:%#018lx\n",i,(page + i)->attribute,(page + i)->PHY_address);
+	}
+
+	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*memory_management_struct.bits_map);
+	color_printk(RED,BLACK,"memory_management_struct.bits_map:%#018lx\n",*(memory_management_struct.bits_map + 1));
+*/
 	while(1)
 		;
 }
--- code/ch04/4-06/kernel/memory.h
+++ code/ch04/4-06/kernel/memory.h
@@ -19,7 +19,6 @@
 #include "printk.h"
 #include "lib.h"
 
-
 //	8Bytes per cell
 #define PTRS_PER_PAGE	512
 
@@ -124,6 +123,8 @@ typedef struct {unsigned long pt;} pt_t;
 #define set_pt(ptptr,ptval)		(*(ptptr) = (ptval))
 
 
+unsigned long * Global_CR3 = NULL;
+
 struct E820
 {
 	unsigned long address;
@@ -139,12 +140,159 @@ struct E820
 struct Global_Memory_Descriptor
 {
 	struct E820 	e820[32];
-	unsigned long 	e820_length;	
+	unsigned long 	e820_length;
+
+	unsigned long * bits_map;
+	unsigned long 	bits_size;
+	unsigned long   bits_length;
+
+	struct Page *	pages_struct;
+	unsigned long	pages_size;
+	unsigned long 	pages_length;
+
+	struct Zone * 	zones_struct;
+	unsigned long	zones_size;
+	unsigned long 	zones_length;
+
+	unsigned long 	start_code , end_code , end_data , end_brk;
+
+	unsigned long	end_of_struct;	
+};
+
+////alloc_pages zone_select
+
+//
+#define ZONE_DMA	(1 << 0)
+
+//
+#define ZONE_NORMAL	(1 << 1)
+
+//
+#define ZONE_UNMAPED	(1 << 2)
+
+////struct page attribute (alloc_pages flags)
+
+//
+#define PG_PTable_Maped	(1 << 0)
+
+//
+#define PG_Kernel_Init	(1 << 1)
+
+//
+#define PG_Referenced	(1 << 2)
+
+//
+#define PG_Dirty	(1 << 3)
+
+//
+#define PG_Active	(1 << 4)
+
+//
+#define PG_Up_To_Date	(1 << 5)
+
+//
+#define PG_Device	(1 << 6)
+
+//
+#define PG_Kernel	(1 << 7)
+
+//
+#define PG_K_Share_To_U	(1 << 8)
+
+//
+#define PG_Slab		(1 << 9)
+
+struct Page
+{
+	struct Zone *	zone_struct;
+	unsigned long	PHY_address;
+	unsigned long	attribute;
+
+	unsigned long	reference_count;
+	
+	unsigned long	age;
+};
+
+
+//// each zone index
+
+int ZONE_DMA_INDEX	= 0;
+int ZONE_NORMAL_INDEX	= 0;	//low 1GB RAM ,was mapped in pagetable
+int ZONE_UNMAPED_INDEX	= 0;	//above 1GB RAM,unmapped in pagetable
+
+#define MAX_NR_ZONES	10	//max zone
+
+/*
+
+*/
+
+struct Zone
+{
+	struct Page * 	pages_group;
+	unsigned long	pages_length;
+	
+	unsigned long	zone_start_address;
+	unsigned long	zone_end_address;
+	unsigned long	zone_length;
+	unsigned long	attribute;
+
+	struct Global_Memory_Descriptor * GMD_struct;
+
+	unsigned long	page_using_count;
+	unsigned long	page_free_count;
+
+	unsigned long	total_pages_link;
 };
 
 
 extern struct Global_Memory_Descriptor memory_management_struct;
 
+unsigned long page_init(struct Page * page,unsigned long flags);
+
+unsigned long page_clean(struct Page * page);
+
 void init_memory();
+
+struct Page * alloc_pages(int zone_select,int number,unsigned long page_flags);
+
+/*
+
+*/
+
+#define	flush_tlb_one(addr)	\
+	__asm__ __volatile__	("invlpg	(%0)	\n\t"::"r"(addr):"memory")
+/*
+
+*/
+
+#define flush_tlb()						\
+do								\
+{								\
+	unsigned long	tmpreg;					\
+	__asm__ __volatile__ 	(				\
+				"movq	%%cr3,	%0	\n\t"	\
+				"movq	%0,	%%cr3	\n\t"	\
+				:"=r"(tmpreg)			\
+				:				\
+				:"memory"			\
+				);				\
+}while(0)
+
+/*
+
+*/
+
+inline unsigned long * Get_gdt()
+{
+	unsigned long * tmp;
+	__asm__ __volatile__	(
+					"movq	%%cr3,	%0	\n\t"
+					:"=r"(tmp)
+					:
+					:"memory"
+				);
+	return tmp;
+}
+
 
 #endif
